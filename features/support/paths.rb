d568e5329c37cc5c166eef4eb7a098369f3526b1
Blurredbits <mmorris00@mac.com>
2013-02-09 13:34:01 -0700

    Added the ability to view a list of all projects

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,9 +10,9 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+
     when /the project page for "([^\"]*)"/
       project_path(Project.find_by_name!($1))
-    
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
@@ -21,10 +21,10 @@ module NavigationHelpers
 
     else
       begin
-        page_name =~ /^the (.*) page$/
+        page_name =~ /the (.*) page/
         path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
-      rescue NoMethodError, ArgumentError
+      rescue Object => e
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
           "Now, go and add a mapping in #{__FILE__}"
       end
